d7a57e1e7fde180504019cd9be41b10b1d5f44b3
Christian Richter <crichter@suse.com>
2021-09-30 16:03:32 +0200

    Add Grub for chainloading
    Signed-off-by: Christian Richter <crichter@suse.com>

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 FROM ubuntu:latest
+ENV TZ=Europe/Berlin
+ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get update \
     && apt-get -y full-upgrade \
     && apt-get -y install \
+    tzdata \
     libssl-dev \ 
     build-essential \
     flex bison \
     git \
+    wget \
     gcc-aarch64-linux-gnu
 
 RUN mkdir -p /data/output
@@ -13,4 +17,7 @@ WORKDIR /data
 RUN git clone https://github.com/u-boot/u-boot
 RUN git clone https://github.com/raspberrypi/firmware
 COPY entrypoint.sh /data/entrypoint.sh
+RUN apt-get -y install \
+    autopoint \
+    python
 ENTRYPOINT /data/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set +ex
 BASEPATH=/data
 echo "--------> Running pi-bootbuilder"
 echo "--------> Cleaning output volume"
@@ -20,8 +21,33 @@ echo "==============> copy u-boot.bin to ${BASEPATH}/output"
 cp u-boot.bin ../output
 popd
 
+echo "--------> Generating copy EFI blob to /boot/EFI"
+BOOTDIR=${BASEPATH}/output/EFI/BOOT
+mkdir -p ${BOOTDIR}/locale
+pushd ${BOOTDIR}
+wget https://download.opensuse.org/ports/aarch64/tumbleweed/repo/oss/EFI/BOOT/bootaa64.efi
+wget https://download.opensuse.org/ports/aarch64/tumbleweed/repo/oss/EFI/BOOT/grub.efi
+wget https://download.opensuse.org/ports/aarch64/tumbleweed/repo/oss/EFI/BOOT/MokManager.efi
+cat <<EOF >"${BOOTDIR}/grub.cfg"
+set btrfs_relative_path="yes"
+search --no-floppy --label rootfs --set=root rootfs
+set prefix=(\$root)/boot/grub2
+configfile (\$root)/boot/grub2/grub.cfg
+EOF
+pushd locale
+wget https://download.opensuse.org/ports/aarch64/tumbleweed/repo/oss/EFI/BOOT/locale/en.mo
+popd && popd
+
 echo "--------> Generating config.txt"
 cat <<EOF > "${BASEPATH}/output/config.txt"
+force_turbo=0
+initial_turbo=30
+over_voltage=0
+dtoverlay=upstream
+dtoverlay=smbios
+arm_freq=840
+core_frequ=375
+sdram_freq=400
 dtoverlay=vc4-kms-v3d
 max_framebuffers=2
 arm_64bit=1
